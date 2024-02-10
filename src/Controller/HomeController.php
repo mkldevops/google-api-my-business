7acc0b835aa530725e817d7610f9cf5dc976e8e7
Fahari <mkl.devops@gmail.com>
2024-02-10 22:39:04 +0000

    feat: #5 set accesstoken on session

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Security\Enum\ServiceAuthEnum;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,9 +13,16 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ROLE_USER')]
 class HomeController extends AbstractController
 {
+    /**
+     * @throws IdentityProviderException
+     */
     #[Route('/', name: 'app_home')]
-    public function __invoke(): Response
+    public function __invoke(ClientRegistry $clientRegistry): Response
     {
+        $client = $clientRegistry->getClient(ServiceAuthEnum::GOOGLE->value);
+        $client->setAsStateless();
+        dump($client);
+
         return $this->render('home/index.html.twig');
     }
 }
